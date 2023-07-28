52708a85b2cfdd8c65c8a01a383d8243d57973d4
umanrai <umanrai58@gmail.com>
2023-07-28 19:28:43 +0545

    Added array

--- index.php
+++ index.php
@@ -1,71 +1,80 @@
 <?php
 
-// String Operator
+/*
+ * What is array and objects
+ */
 
-// Php is a dynamic type language.
+$args = []; // Short syntax array
 
-$uman = "Uman Rai"; // string
+$args2 = array();
 
-$uman = 1; // Integer
+$args = ["dsdas", "asdasda"];
 
-$uman = 1; // Decimal/Float
+$dynamicItem = 'Uman';
 
-$rai = '2'; // Integer
+$args[] = $dynamicItem;
+$args[] = 'Sachin';
+$args[] = 'kin';
+echo "<br/>";
 
-echo $uman + $rai;
+// Args is a single dimensional array
+$sd = [ 'uman0', 'Uman1', 'Uman2', 'Uman3' , 'UManrai']; // 4
 
-echo "<br />";
-
-// Arithmetic Operators
-echo (1 * 2) + (2 * 2); // [BADMAS]
-
-echo "<br />";
-
-$rai = $rai + 5;
-
-echo $rai += $rai; // $rai = ($rai + $rai)
-
-echo "<br />";
-
-$a = 1;
-
-//echo $a++; // Post-increment operator
-
-echo ++$a; // Pre-increment operator
+//echo $sd[0];
+//echo $sd[1];
+//echo $sd[2];
+//echo $sd[3];
+//echo $sd[4];
 
 echo "<br />";
-// Comparison operator
-
-$x = 10;
-
-$y = '10';
-
-if ($x === $y) { // Type coercion, strict type
-    echo 'Equal';
-} else {
-    echo 'Not Equal';
+foreach($sd as $sds) {
+    echo $sds . "<br />";
 }
 
-if ($x != $y) {
-    echo "X is not equals to Y";
+
+echo "<br />";
+
+// Multi dimensional array
+$md = [
+    [
+        'name' => 'Uman',
+        'value' => 'value1',
+        'age'  => 20,
+        'caste' => 'rai1'
+    ],
+
+    [
+        'name' => 'Sachin',
+        'value' => 'value1',
+        'age' => 23,
+        'caste' => 'rai'
+    ],
+];
+
+//echo $md[0]['caste'];
+
+foreach ($md as $mds) {
+    echo $mds['name'] . $mds['caste'] . $mds['age']. "<br>";
+}
+echo "<br />";
+foreach ($md as $mds) {
+    foreach ($mds as $key => $value) {
+        echo $key . ' = ' . $value . "<br>";
+    }
 }
 
+
+//// Creating a multi dimensional array
+//$multiargs = [];
 //
-
-echo "<br />";
-
-// And Operator Syntax (&&, and)
-
-$c = 12;
-
-$d = 25;
-
-if ($c > 10 && $d > 20) {
-    echo "Yes thet are greater";
-}
-echo "<br />";
-// Or Operator syntax (||, or)
-
-if ($c > 10 || $d > 20) {
-    echo "Yes thet are greater";
-}
+//foreach ($args as $index => $arg) {
+//    $multiargs[] = [
+//        'value' => $arg,
+//        'index' => $index
+//    ];
+//}
+//echo "<br/>";
+//
+//echo "<pre>";
+//var_dump($multiargs);
+//echo "</pre>";
