0a48c71fb081b5110783adb719b4a12ec152a641
umanrai <umanrai58@gmail.com>
2023-07-31 14:05:21 +0545

    Added conditions

--- index.php
+++ index.php
@@ -7,78 +7,42 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/main.css">
 
-    <title>Document</title>
+    <title>Condition</title>
 </head>
 <body>
 
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    <input class="size" type="number" name="num01" placeholder="Number one"><br>
-    <select class="size" id="dropdown" name="operator">
-        <option value="add">+(Addition)</option>
-        <option value="subtract">-(Subtraction)</option>
-        <option value="multiply">*(Multiplication)</option>
-        <option value="divide">/(Division)</option>
-    </select><br>
-    <input class="size"  type="number" name="num02" placeholder="Number two"><br>
-    <button class="size">Calculate</button>
-</form>
+<?php
 
-<div class="container">
-    <?php
+    $currentYear = date('Y');
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        /*$num01 = $_POST["num01"];
-        $num02 = $_POST["num02"];*/
-
-        // Sanitizing Or stopping users for providing wrong data
-        // Grab data from inputs
-
-        $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
-        $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
-        $operator = htmlspecialchars($_POST["operator"]);
-
-        // Error handlers : it prevents users, from submitting empty inputs
-        $errors = false;
-        // it is checking whether inputs are empty or not
-        if (empty($num01) || empty($num02) || empty($operator)) {
-            echo "<p class='calc-error'>Fill in all fields!</p>";
-            $errors = true;
-        }
-
-        // it is checking whether inputs are numbers or not
-        if (!is_numeric($num01) || !is_numeric($num02)) {
-            echo "<p class='calc-error'>Only write numbers!</p>";
-            $errors = true;
-        }
-
-
-        // calculate the numbers if no errors
-        if (!$errors){
-            $value = 0;
-            switch ($operator) {
-                case "add":
-                    $value = $num01 + $num02;
-                    break;
-                case "subtract":
-                    $value = $num01 - $num02;
-                    break;
-                case "multiply":
-                    $value = $num01 * $num02;
-                    break;
-                case "divide":
-                    $value = $num01 / $num02;
-                    break;
-                default:
-                    echo "<p class='calc-error'>Something went wrong!</p>";
-            }
-
-            echo "<p class='calc-result'>Result = " . $value ."</p>";
-        }
+    if ($currentYear == 2023) {
+      echo "Current Year ";
+    } else {
+      echo 'NO year';
     }
-    ?>
-</div>
 
+    echo nl2br("\n");
 
+    echo $currentYear == 2023 ? 'Current Year' : 'No Year';
+    echo nl2br("\n");
+
+    if ($currentYear == 2023) :
+      echo "Current Year ";
+    else :
+      echo 'NO year';
+    endif;
+
+    echo nl2br("\n");
+
+    $uyman = 'NoYear';
+
+    if ($currentYear == 2023) {
+      $uyman = "Current Year ";
+    }
+
+    echo $uyman;
+
+?>
 
 </body>
 
