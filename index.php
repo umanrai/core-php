172a9825eefe6e9b05708a9f32afa4a784c8518a
umanrai <umanrai58@gmail.com>
2023-08-01 14:43:12 +0545

    Added loops

--- index.php
+++ index.php
@@ -7,28 +7,54 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/main.css">
 
-    <title>Constants</title>
+    <title>Loops</title>
 </head>
 <body>
 
 <?php
-    // Constants mean constant, it won't change.
-    // Variable name always should be capital.
-    define("PI", 3.14);
-    define ("IS_Uman", "RAM");
-    echo PI . "<br />";
-
-    define("NAME", "uman");
-    echo NAME . "<br />";
-
-    function test()
+    // For loop
+    /*for ($i = 1; $i <=5; $i++)
     {
-        echo IS_Uman;
+        echo "This is iteration number " . "$i" . "<br />";
     }
-    test();
+
+    $test1 = 2;
+    $test2 = 6;
+    for ($i = $test1; $i <= $test2; $i++)
+    {
+        echo "This iteration number is " . "$i" . "<br />";
+    }*/
+
+
+    // While loop
+    /*$test = 5;
+    while ($test < 10)
+    {
+        echo $test . "<br />";
+        $test++;
+    }*/
+
+    // Do-While loop - no matter what it will always loop it out once
+    /*$test = 11;
+    do {
+        echo $test . "<br />";
+        $test++;
+    } while ($test < 10);*/
+
+    // foreach loop
+    /*$fruits = array("Apple", "Banana", "Orange");
+    foreach ($fruits as $fruit)
+    {
+        echo "This is a " . $fruit . "<br />";
+    }*/
+
+    $fruits = [ "Apple" => "Red", "Banana" => "Yellow", "Orange" => "Orange" ];
+    foreach ($fruits as $fruit => $color)
+    {
+        echo "This is a " . $fruit . "," . "that has a color of " ."$color" . "<br />";
+    }
 
 ?>
-
 </body>
 
 </html>
